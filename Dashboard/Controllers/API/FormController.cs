b86a36cd5d9fa2409b99445372116b90923ca67f
Stu <stu@ironiclensflare.com>
2014-07-13 08:36:39 +0100

    Messed about with Cassette before giving up

---
+++ Dashboard/Controllers/API/FormController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dashboard.Models;
+
+namespace Dashboard.Controllers.API
+{
+    public class FormController : ApiController
+    {
+        private DashboardFormSubmissions db = new DashboardFormSubmissions();
+
+        //POST: /api/form/submission
+        public HttpResponseMessage PostSubmission()
+        {
+            int formID;
+
+            try
+            {
+                formID = Convert.ToInt16(System.Web.HttpContext.Current.Request.Headers["FormID"]);
+            }
+            catch (Exception)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                FormSubmission f = new FormSubmission();
+                f.FormID = formID;
+
+                db.FormSubmissions.Add(f);
+                db.SaveChanges();
+
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+
+            catch (Exception)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+
+        //GET: /api/form/submission
+        public HttpResponseMessage GetSubmission()
+        {
+            return new HttpResponseMessage(HttpStatusCode.NotImplemented);
+        }
+    }
+}
--- Dashboard/Controllers/HomeController.cs
+++ Dashboard/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace Dashboard.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Help()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
