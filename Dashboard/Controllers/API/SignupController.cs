0826c33fafa3a3ce2f6a51a2222414b56f726132
Stu <stu@ironiclensflare.com>
2014-08-09 09:56:49 +0100

    Started work on GD middleware

---
+++ Dashboard/Controllers/API/SignupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dashboard.Controllers.API
+{
+    public class SignupController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            IEnumerable<string> headerValues = Request.Headers.GetValues("Email");
+            var email = headerValues.FirstOrDefault();
+
+            
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+    }
+}
